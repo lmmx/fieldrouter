ea065d4f2c14dcea53cb1171c63dd0f4a741a36f
Louis Maddox <louismmx@gmail.com>
2024-08-16 12:15:29 +0100

    feat: initial commit

---
+++ examples/nested.py
@@ -0,0 +1,24 @@
+from fieldrouter import R, Route, Routed, Router
+
+__all__ = ("Where", "What")
+
+
+class Where(Router):
+    field_1: Route = "a.aa.aaa.0"
+    field_2: Route = "a.aa.aaa.1"
+    field_3: Route = "b.bb.2"
+
+
+class What(Routed[R], extra="forbid"):
+    field_1: str
+    field_2: str
+    field_3: int
+
+
+json_str = '{"a":{"aa":{"aaa":["value_1","value_2"]}},"b":{"bb":[3,4,5]}}'
+result = What[Where].model_validate_json(json_str)
+
+result_dict = result.model_dump()
+print(result_dict)
+
+assert result_dict == {"field_1": "value_1", "field_2": "value_2", "field_3": 5}
---
+++ examples/simple.py
@@ -0,0 +1,18 @@
+from fieldrouter import R, Route, Routed, Router
+
+
+class Where(Router):
+    some_value: Route = "path.to.the.value.0"
+
+
+class What(Routed[R]):
+    some_value: int
+
+
+data = {"path": {"to": {"the": {"value": [100]}}}}
+result = What[Where].model_validate(data)
+
+print(result.model_dump())  # {'some_value': 100}
+
+assert result.some_value == 100
+assert result.model_dump() == {"some_value": 100}
