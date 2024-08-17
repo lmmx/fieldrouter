e9983dd07fb850526c9e0cbd660fc1a4a89a6671
Louis Maddox <louismmx@gmail.com>
2024-08-17 14:26:37 +0100

    chore: document availability of kwargs

--- examples/simple.py
+++ examples/simple.py
@@ -1,16 +1,19 @@
 from fieldrouter import Routing, RoutingModel
 
 
+# You can specify the Routing type annotation with or without keyword arguments
 class DataRouter(RoutingModel):
-    my_value: Routing(int, "path.to.the.value.0")
+    a_value: Routing(int, "path.to.the.value.0")
     with_via: Routing(int, via="path.to.the.value.0")
+    with_kws: Routing(tp=int, via="path.to.the.value.0")
 
 
 data = {"path": {"to": {"the": {"value": [100]}}}}
 result = DataRouter.model_validate(data)
 
-print(result.model_dump())  # {'my_value': 100, 'with_via': 100}
+print(result.model_dump())  # {'a_value': 100, 'with_via': 100, 'with_kws': 100}
 
-assert result.my_value == 100
+assert result.a_value == 100
 assert result.with_via == 100
-assert result.model_dump() == {"my_value": 100, "with_via": 100}
+assert result.with_kws == 100
+assert result.model_dump() == {"a_value": 100, "with_via": 100, "with_kws": 100}
