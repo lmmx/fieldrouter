e43b6aa0349c8b902ef253c86ca72f8cf03bdbb0
Louis Maddox <louismmx@gmail.com>
2024-08-17 14:22:17 +0100

    test: demonstrate that via param is optional

--- examples/simple.py
+++ examples/simple.py
@@ -2,13 +2,15 @@ from fieldrouter import Routing, RoutingModel
 
 
 class DataRouter(RoutingModel):
-    some_value: Routing(int, via="path.to.the.value.0")
+    my_value: Routing(int, "path.to.the.value.0")
+    with_via: Routing(int, via="path.to.the.value.0")
 
 
 data = {"path": {"to": {"the": {"value": [100]}}}}
 result = DataRouter.model_validate(data)
 
-print(result.model_dump())  # {'some_value': 100}
+print(result.model_dump())  # {'my_value': 100, 'with_via': 100}
 
-assert result.some_value == 100
-assert result.model_dump() == {"some_value": 100}
+assert result.my_value == 100
+assert result.with_via == 100
+assert result.model_dump() == {"my_value": 100, "with_via": 100}
