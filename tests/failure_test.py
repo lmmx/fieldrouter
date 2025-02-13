f4f65a7dadb677784c82079c6063aa5b8c0c4ef3
Louis Maddox <louismmx@gmail.com>
2025-02-13 14:51:20 +0000

    style: lint

--- tests/failure_test.py
+++ tests/failure_test.py
@@ -7,7 +7,7 @@ def test_bad_route():
         a_value: Routing(int, "path.to.the.value.MISTYPE_MISS")
 
     data = {"path": {"to": {"the": {"value": [100]}}}}
-    result = DataRouter.model_validate(data)
+    DataRouter.model_validate(data)
 
 
 if __name__ == "__main__":
